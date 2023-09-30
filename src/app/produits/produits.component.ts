9f665d09d6bf0879d8d662a197fbb2e66b8afac8
nazis1282 <nassaziz85@gmail.com>
2023-10-01 00:24:38 +0100

    tp01

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
+})
+export class ProduitsComponent implements OnInit {
+
+  produits : string[]
+
+  constructor() {
+
+    this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}
